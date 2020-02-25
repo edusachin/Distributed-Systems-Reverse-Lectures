92006be6ce219bf6d20ce065c1bdaa4662774935
Sachin Karve <sachin.karve@sjsu.edu>
2020-02-24 17:14:10 -0800

    final update

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ const app = express();
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
-
+const compression = require('compression')
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
+
+app.use((compression()))
 
 
 const userstorage = multer.diskStorage({
@@ -20,11 +22,15 @@ const userstorage = multer.diskStorage({
 
 const useruploads = multer({
     storage: userstorage,
-    limits: { fileSize: 10000000000000000 },
+    limits: { fileSize: 100000000000 },
 }).single("image");
 
-
+//1048576
 app.post("/normal", (req, res) => {
+console.log('received');
+    // res.setHeader('Content-Type', 'text/event-stream');
+    // res.setHeader('Cache-Control', 'no-cache');
+
     useruploads(req, res, function (err) {
         if (!err) {
             res.status(200).send("UPLOADED");
@@ -50,9 +56,6 @@ app.post("/compressed", (req, res) => {
         }
     })
 });
-
-
-
 
 
 app.listen(3001, () => {
